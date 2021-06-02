89386f1a51205e855ed7f8337e6264d6f0ec7c23
MasayukiU <68602249+MasayukiU@users.noreply.github.com>
2021-06-03 04:49:09 +0900

    Revert "first commit"
    This reverts commit c3a92a6d1a29d8d6d95b53cff32659c357ff633d.

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,12 +1,11 @@
 class Prototype < ApplicationRecord
 	belongs_to :user
-	has_many :comments, dependent: :destroy
+	has_many :comments
 
 	has_one_attached :image
 
+	validates :image, presence: true
 	validates :catch_copy, presence: true
-	validates :title,      presence: true
-	validates :concept,    presence: true
-	validates :image,      presence: true
+	validates :concept, presence: true
 end
 
