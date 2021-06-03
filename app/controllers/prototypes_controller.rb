cd1525497b2ad50f86610bdb1d1aa483b893d893
MasayukiU <68602249+MasayukiU@users.noreply.github.com>
2021-06-03 14:05:15 +0900

    update変更

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -28,21 +28,19 @@ class PrototypesController < ApplicationController
 	def edit
 	end
 
-	def update
-   prototype = Prototype.find(params[:id])
-   	if prototype.update(prototype_params)
-			redirect_to prototype_path(prototype.id)
-		else
-			render :edit
-		end
-	end
+  def update
+    @prototype = Prototype.find(params[:id])
+    if @prototype.update(prototype_params)
+      redirect_to prototype_path
+    else
+      render :edit
+    end
+  end
 
 	def destroy
 		prototype = Prototype.find(params[:id]) 
 		if prototype.destroy
       redirect_to root_path
-    else
-      render :show
 	  end
 	end
 
